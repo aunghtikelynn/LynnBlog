2c83221ab09861a8dc04e1c5caa74f9cbf5abaf5
aunghtikelynn <aunghtikelynn@gmail.com>
2024-11-30 22:28:59 +0630

    blogHW

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',[App\Http\Controllers\FrontController::class, 'blog'])->name('blog');
+
+Route::get('/blog-post/{id}',[App\Http\Controllers\FrontController::class, 'blogPost'])->name('blog-post');
+
+Route::group(['prefix'=>'backend','as'=>'backend'],function(){
+    Route::get('/',[App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
